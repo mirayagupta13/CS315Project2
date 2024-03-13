87cb23922ac349b515e018495caff5a56dd52cb8
Miles <mlsmzk02@gmail.com>
2024-03-13 04:28:32 -0400

    Get results for chunk 4 pyktok data collection

--- cs315project2datacollection/pyktok-collect.py
+++ cs315project2datacollection/pyktok-collect.py
@@ -21,7 +21,7 @@ def collect_metadata(inputFile, outputFile):
     
     pyk.specify_browser('chrome')
     pyk.save_tiktok_multi_urls(urls,  # list of URLs to visit
-                               True, # don't save videos   
+                               False, # don't save videos is False
                 		       outputFile, # csv file
                 		       5) # max time sleep
     
